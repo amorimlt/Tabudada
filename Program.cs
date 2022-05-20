7ae190f5c3d111b24abbe95d8c798cab1c509a29
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-05-20 20:18:33 -0300

    finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿int contador, numero;
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("===BEM VINDO A CALCULADORA!===\n");
+Thread.Sleep(2000);
+Console.ResetColor();
+
+Console.WriteLine("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine(" ");
+
+contador = 0;
+
+if(numero <= 0){
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("Digite outro número.");
+    Console.ResetColor();
+
+}
+else{
+    Console.WriteLine($"Número Selecionado: {numero}");
+    while (contador <= 10){
+        int resultado = contador * numero;
+
+        Console.WriteLine($"{numero} * {contador++} = {resultado}");
+
+    }
+    Console.WriteLine(" ");
+}
